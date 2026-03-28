841c3e4d494d675585975232d17e477144792bad
Benevanio Santos <benevaniosantos930@gmail.com>
2026-03-28 19:38:07 -0300

    feat: add Swagger documentation setup with swagger-jsdoc and swagger-ui-express

--- src/common/infrastructure/http/app.ts
+++ src/common/infrastructure/http/app.ts
@@ -1,11 +1,26 @@
 import cors from 'cors'
 import express, { NextFunction, Request, Response } from 'express'
+import swaggerJSDoc from 'swagger-jsdoc'
+import SwaggerUI from 'swagger-ui-express'
 import { errorHandler } from './middlewares/errorHandler'
 import { routes } from './router'
+
+const swaggerSpec = swaggerJSDoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'api ecommerce',
+      version: '1.0.0',
+      description: 'API para gerenciamento de vendas',
+    },
+  },
+  apis: [],
+})
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use('/docs', SwaggerUI.serve, SwaggerUI.setup(swaggerSpec))
 app.use(routes)
 app.use((err: Error, req: Request, res: Response, next: NextFunction) =>
   errorHandler(err, req, res, next),
