1a675bbad7f2ca6e30958e11928fba3816f9214c
Benevanio Santos <benevaniosantos930@gmail.com>
2026-03-28 15:36:34 -0300

    feat: add routing functionality with a welcome message

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
+import { routes } from './router'
 const app = express()
 dotenv.config()
 const Port = process.env.PORT || 3000
@@ -8,6 +9,7 @@ const Port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
+app.use(routes) 
 app.listen(Port, () => {
   console.log(`Server is running on port ${Port}`)
 })
