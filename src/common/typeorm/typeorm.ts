4e4adef415f5392a3c68a9cd54cde679c90751ec
Benevanio Santos <benevaniosantos930@gmail.com>
2026-03-29 06:58:42 -0300

    feat: add TypeORM data source configuration for PostgreSQL

---
+++ src/common/typeorm/typeorm.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm'
+import { env } from '../infrastructure/http/env'
+
+export const dataSource = new DataSource({
+  type: 'postgres' as const,
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  username: env.DB_USER,
+  password: env.DB_PASS,
+  database: env.DB_NAME,
+  schema: env.DB_SCHEMA,
+  synchronize: true,
+  logging: false,
+  entities: [],
+  migrations: [],
+  subscribers: [],
+})
