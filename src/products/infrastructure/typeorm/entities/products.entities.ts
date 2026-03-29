cbb01c7d0b398dd5130f908a648b0cfa11dee5ba
Benevanio Santos <benevaniosantos930@gmail.com>
2026-03-29 15:45:52 -0300

    feat: enhance ProductEntity with additional columns for timestamps

--- src/products/infrastructure/typeorm/entities/products.entities.ts
+++ src/products/infrastructure/typeorm/entities/products.entities.ts
@@ -1,10 +1,22 @@
 import { ProductModel } from '@/products/domain/models/products.model'
+import { Column, PrimaryGeneratedColumn } from 'typeorm'
 
 export class ProductEntity implements ProductModel {
+  @PrimaryGeneratedColumn('uuid')
   id: string
+
+  @Column('varchar')
   name: string
+
+  @Column('decimal')
   price: number
+
+  @Column('int')
   quantity: number
+
+  @Column('timestamp')
   created_at: Date
+
+  @Column('timestamp')
   updated_at: Date
 }
