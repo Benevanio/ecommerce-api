357f3114ef92cda15dfd16f94fcaa50743b25ddd
Benevanio Santos <benevaniosantos930@gmail.com>
2026-04-19 14:22:40 -0300

    test: implement test for successful product creation in CreateProductUseCase

--- src/products/application/usecase/create-product.usecase.spec.ts
+++ src/products/application/usecase/create-product.usecase.spec.ts
@@ -1,7 +1,6 @@
 import { InMemoryProductsRepository } from '@/products/infrastructure/in-memory/repositories/in-memory.repository.products'
 import { ProductsRepository } from '@/products/repositories/product.repository'
-import { describe } from '@jest/globals'
-import { beforeEach, it } from 'node:test'
+import { beforeEach, describe, expect, it } from '@jest/globals'
 import { CreateProductUseCase } from './create-product.usecase'
 
 describe('Create Product Use Case', () => {
@@ -12,5 +11,19 @@ describe('Create Product Use Case', () => {
     suit = new CreateProductUseCase.Usecase(repoository)
   })
 
-  it('should create a product successfully', () => {})
+  it('should create a product successfully', () => {
+    const input: CreateProductUseCase.input = {
+      name: 'Test Product',
+      price: 100,
+      quantity: 10,
+    }
+    suit.execute(input).then(output => {
+      expect(output).toHaveProperty('id')
+      expect(output.name).toBe(input.name)
+      expect(output.price).toBe(input.price)
+      expect(output.quantity).toBe(input.quantity)
+      expect(output).toHaveProperty('created_at')
+      expect(output).toHaveProperty('updated_at')
+    })
+  })
 })
