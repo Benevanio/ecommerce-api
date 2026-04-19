19d34af17f34d0e1e8762f6a70c6e6be3c7bd2b2
Benevanio Santos <benevaniosantos930@gmail.com>
2026-04-19 14:08:48 -0300

    fix: update ProductModel to make created_at and updated_at optional

--- src/products/domain/models/products.model.ts
+++ src/products/domain/models/products.model.ts
@@ -3,8 +3,8 @@ export interface ProductModel {
   name: string
   price: number
   quantity: number
-  created_at: Date
-  updated_at: Date
+  created_at?: Date
+  updated_at?: Date
 }
 
 export interface ProductsRepository {
